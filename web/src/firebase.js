9fa3aef73bdee689d453042dd0c1896370cbf9d5
Prathik-09 <prathikpatil02@gmail.com>
2025-07-04 15:16:07 +0530

    HomePage added

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXDY8ikdIhhl8STbXXJVVkORIbVQfUuNw",
-  authDomain: "webcafeland-3cc3b.firebaseapp.com",
-  projectId: "webcafeland-3cc3b",
-  storageBucket: "webcafeland-3cc3b.firebasestorage.app",
-  messagingSenderId: "351638663633",
-  appId: "1:351638663633:web:beffff4fcc3f661747f0f0",
-  measurementId: "G-EWNHQQ45CB"
+  apiKey:process.env.REACT_APP_API_KEY ,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId:process.env.REACT_APP_MEASUREMENT_ID 
 };
 
 // Initialize Firebase
